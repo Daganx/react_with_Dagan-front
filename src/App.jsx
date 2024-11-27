f3b54cb38027d1da654f61944e253527136f6399
Dagan <114420715+Daganx@users.noreply.github.com>
2024-11-27 19:31:34 +0100

    update import app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./components/protectedRoute/ProtectedRoute";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Dashboard from "./pages/dashboard";
 import IndexPage from "./pages/index";
 import ArticleDetail from "./components/articles/articleDetail/ArticleDetail";
